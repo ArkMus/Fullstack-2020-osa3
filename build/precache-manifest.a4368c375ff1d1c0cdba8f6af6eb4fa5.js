dc35add22c908155880040fe990db12f6438d7f4
Arkmus <markus.andersson97@gmail.com>
2020-02-04 17:12:33 +0200

    Build added.

---
+++ build/precache-manifest.a4368c375ff1d1c0cdba8f6af6eb4fa5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b0645137c625a36b7e2ebdf61806f770",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ab56c5793baa1f3f2992",
+    "url": "/static/css/main.b5857471.chunk.css"
+  },
+  {
+    "revision": "a4ff7f15b0a9c86bc646",
+    "url": "/static/js/2.b60c7687.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.b60c7687.chunk.js.LICENSE"
+  },
+  {
+    "revision": "ab56c5793baa1f3f2992",
+    "url": "/static/js/main.aceafe5b.chunk.js"
+  },
+  {
+    "revision": "cf9fd95ce589cae23005",
+    "url": "/static/js/runtime-main.2e9fb530.js"
+  }
+]);
